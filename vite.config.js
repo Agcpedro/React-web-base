e7dd036058f8744fd4f557beaa9c93bad0e3b1f1
PEDRO AMANDO GANDOS CITELLI <pedro.citelli@csn.com.br>
2024-06-07 07:52:26 -0300

    Primeiro commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  //Here we Switch the port of the local host
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
